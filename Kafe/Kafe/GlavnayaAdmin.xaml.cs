de4b182512a5327dbdb433896677c7bbb25d11eb
bersik-dot <138628977+bersik-dot@users.noreply.github.com>
2023-12-17 22:47:23 +0300

    1

---
+++ Kafe/Kafe/GlavnayaAdmin.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Kafe
+{
+    /// <summary>
+    /// Логика взаимодействия для GlavnayaAdmin.xaml
+    /// </summary>
+    public partial class GlavnayaAdmin : Page
+    {
+        public GlavnayaAdmin()
+        {
+            InitializeComponent();
+        }
+    }
+}
